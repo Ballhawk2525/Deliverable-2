108717883dc20fde14f186bf8e4314a925b7eed8
idris <idrishobdy@yahoo.com>
2019-09-05 01:55:56 -0400

    forms added

---
+++ script.js
@@ -0,0 +1,22 @@
+var eventType;
+var tempFahr;
+
+if (eventType = casual) {
+    
+} else if (eventType = semi-formal) {
+
+} else if (eventType = formal) {
+
+} else {
+
+}
+
+if (tempFahr < 54) {
+    
+} else if (tempFahr > 54 || < 70) {
+    
+} else if (tempFahr > 74) {
+    
+} else {
+    
+}
