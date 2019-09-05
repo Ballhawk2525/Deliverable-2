f57c98430bf8055579fab00546faf5305b4fcd70
idris <idrishobdy@yahoo.com>
2019-09-05 08:05:28 -0400

    element by id added

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 var eventType;
 var tempFahr;
 
+function myfunction () {
+    document.getElementById("demo").innerHTML = 5 + 6; 
+}
 if (eventType = casual) {
-    
+    console.log ("something comfy")
 } else if (eventType = semi-formal) {
-
+    prompt ("a polo")
 } else if (eventType = formal) {
-
+    prompt ("a suit")
 } else {
 
 }
 
 if (tempFahr < 54) {
-    
-} else if (tempFahr > 54 || < 70) {
-    
+    console.log ("a coat")
+} else if (tempFahr > 54 && tempFahr < 70) {
+    propmt ("a jacket")
 } else if (tempFahr > 74) {
-    
+    prompt ("no jacket")
 } else {
-    
+
 }
