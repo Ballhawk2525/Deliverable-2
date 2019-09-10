b241906fe9d1f79f0bf9abf919a340d8577ef833
idris <idrishobdy@yahoo.com>
2019-09-10 09:53:18 -0400

    concatenation changed

--- script.js
+++ script.js
@@ -7,27 +7,19 @@ var question2 = undefined;
 
 if (eventType === "casual") {
     question1 = "something comfy";
-} 
-
-if (eventType === "semi-formal") {
+} else if (eventType === "semi-formal") {
     question1 = "a polo";
-} 
-
-if (eventType === "formal") {
+} else if (eventType === "formal") {
     question1 = "a suit";
 }
 
 if (tempFahr < 54) {
     question2 = "a coat";
-} 
-
-if (tempFahr >= 54 && tempFahr <= 70) {
+} else if (tempFahr >= 54 && tempFahr <= 70) {
      question2 = "a jacket";
-} 
-
-if (tempFahr > 70) {
+} else if (tempFahr > 70) {
      question2 = "no jacket";
 } 
 
-result = 
+result = "Because it is " + tempFahr + " outside and you are going to a " + eventType + " event, you should wear " + question1 + " and " + question2; 
 console.log(result)
