9a92a9e07c7bb25ed3c6e2952b25c552877974a2
idris <idrishobdy@yahoo.com>
2019-09-10 02:25:33 -0400

    parenthesis removed and semi colons added

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-let eventType = prompt ("What type of event are you attending? ");
+var eventType = prompt ("What type of event are you attending? ");
 let tempFahr = prompt ("What is the temperature in degrees?");
 
-let result = undefined;
-let question1 = undefined;
-let question2 = undefined;
+var result = undefined;
+var question1 = undefined;
+var question2 = undefined;
 
 if (eventType === "casual") {
-    question1 = ("something comfy")
+    question1 = "something comfy";
 } else if (eventType === "semi-formal") {
-    question1 = ("a polo");
+    question1 = ;"a polo";
 } else if (eventType === "formal") {
-    question1 = ("a suit");
+    question1 = "a suit";
 }
 
-if (temperature < 54) {
-    ("a coat")
-} else if (temperature > 54 && temperature < 70) {
-     ("a jacket")
-} else if (temperature > 70) {
-     ("no jacket")
+if (tempFahr < 54) {
+    question2 = "a coat";
+} else if (tempFahr >= 54 && temperature <= 70) {
+     question2 = "a jacket";
+} else if (tempFahr > 70) {
+     question2 = ;"no jacket";
 } 
 
+result= "Accounting for a" + tempFahr + "day, the usual dress for a" + eventType + "event is usually" + question1 + question2;
+console.log(result)
