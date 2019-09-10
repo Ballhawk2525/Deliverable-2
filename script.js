340d2a7798feb5af94b2e6e4695d97cc335182e4
idris <idrishobdy@yahoo.com>
2019-09-10 08:36:56 -0400

    button removed and concatenation chnaged

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var eventType = prompt ("What type of event are you attending? ");
-let tempFahr = prompt ("What is the temperature in degrees?");
+var tempFahr = prompt ("Enter temperature"); 
 
 var result = undefined;
 var question1 = undefined;
@@ -7,19 +7,27 @@ var question2 = undefined;
 
 if (eventType === "casual") {
     question1 = "something comfy";
-} else if (eventType === "semi-formal") {
-    question1 = ;"a polo";
-} else if (eventType === "formal") {
+} 
+
+if (eventType === "semi-formal") {
+    question1 = "a polo";
+} 
+
+if (eventType === "formal") {
     question1 = "a suit";
 }
 
 if (tempFahr < 54) {
     question2 = "a coat";
-} else if (tempFahr >= 54 && temperature <= 70) {
-     question2 = "a jacket";
-} else if (tempFahr > 70) {
-     question2 = ;"no jacket";
 } 
 
-result= "Accounting for a" + tempFahr + "day, the usual dress for a" + eventType + "event is usually" + question1 + question2;
+if (tempFahr >= 54 && tempFahr <= 70) {
+     question2 = "a jacket";
+} 
+
+if (tempFahr > 70) {
+     question2 = "no jacket";
+} 
+
+result = 
 console.log(result)
